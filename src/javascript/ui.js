0698957fe67759469b11bc731264b2a4522dc13b
Joseph <joseph@verifiedjoseph.com>
2017-10-03 18:28:55 +0100

    Updated to versions 5.1.2 & 1.1.2 (Firefox)
    Minor code changes.
    Updated archive.js and request.js to work with changes to http headers
    in http/2
    Fixed timeSince() function not converting datetime to local time zone in
    format.js
    Added version zip files
    Added Google Chrome stats file

--- src/javascript/ui.js
+++ src/javascript/ui.js
@@ -7,8 +7,7 @@
 	Create, update or remove elements 
  */
 function UI() {
-	var self = this,
-		domElement = null;
+	var domElement = null;
 
 	/**
 	 * Does the element exist in the DOM
@@ -19,38 +18,38 @@ function UI() {
 
 		var status = false;
 		domElement = null;
-		
+
 		if (element !== 'undefined' || element !== null) {
-		
+
 			domElement = document.getElementById(element);
-			
+
 			if (domElement !== null) { // Element found
-				
+
 				status = true;
 
 			} else {
-				
+
 				debug.log('Element not Found:' + element);
-				
+
 			}
 
 		}
-		
+
 		return status;
-		
+
 	};
-	
+
 	/**
 	 * Update the element's contents (text)
 	 * @param {string} element - element id
 	 * @param {string} data
 	 */
 	this.content = function content(element, value) {
-	
+
 		if (this.isElement(element) === true && value !== undefined) {
-			
+
 			domElement.textContent = value;
-	
+
 		}
 
 	};
@@ -61,68 +60,68 @@ function UI() {
 	 * @param {string} data
 	 */
 	this.visibility = function visibility(element, action) {
-	
+
 		if (this.isElement(element) === true) {
 
 			if (action === 'show') {
-				
+
 				domElement.style.display = 'block';
 
 			} else if (action === 'hide') {
-				
+
 				domElement.style.display = 'none';
-			
+
 			}
 
 		}
 
 	};
-	
+
 	/**
 	 * Change or add an element's class
 	 * @param {string} element - element id
 	 * @param {string} value - class name
 	 */
 	this.className = function className(element, value) {
-		
+
 		if (this.isElement(element) === true) {
-			
+
 			domElement.className = value;
-		
+
 		}
 
 	};
-	
+
 	/**
 	 * Disable a input box or button
 	 * @param {string} element - element id
 	 * @param {boolean} value
 	 */
 	this.disableInput = function disableInput(element, value) {
-		
+
 		if (this.isElement(element) === true) {
-		
+
 			domElement.disabled = value;
-		
+
 		}
 
 	};
-	
+
 	/**
 	 * Set or change the contents of an element's title
 	 * @param {string} element - element id
 	 * @param {boolean} value
 	 */
 	this.title = function title(element, value) {
-		
+
 		if (this.isElement(element) === true && typeof value !== 'undefined') {
-			
+
 			domElement.title = value;
-		
+
 		}
 
 	};
-	
+
 	/**
 	 * Set or change the contents of an element's attribute
 	 * @param {string} element - element id
@@ -130,32 +129,32 @@ function UI() {
 	 * @param {string} value - attribute value
 	 */
 	this.attribute = function attribute(element, attr, value) {
-		
+
 		if (this.isElement(element) === true && typeof attr !== 'undefined' && typeof value !== 'undefined') {
-			
+
 			domElement.setAttribute(attr, value);
-		
+
 		}
 
 	};
-	
+
 	/**
 	 * Remove an attribute from an element  
 	 * @param {string} element - element id
 	 * @param {string} attr - attribute name
 	 */
 	this.attributeRemove = function attributeRemove(element, attr) {
-		
+
 		if (this.isElement(element) === true && typeof attr !== 'undefined') {
 
 			if (domElement.hasAttribute(attr)) {
-				
+
 				domElement.removeAttribute(attr);
-			
+
 			}
 
 		}
 
 	};
-	
+
 }
