8e5caa947542f288f7fe1958eec71f33bedcbff9
Joseph <joseph@verifiedjoseph.com>
2018-03-21 14:15:06 +0000

    Added URL and IPv4 validation regex
    Added URL and IPv4 validation regex
    Added localhost and archive.org to hostNameBlackList array
    Removed old URL path regex

--- src/javascript/global.js
+++ src/javascript/global.js
@@ -12,12 +12,15 @@ var global = {
 		api: 'https://archive.org/wayback/available?url='
 	},
 	regex: {
+		
 		// URL validation
 		url: /^https?:\/\/[a-zA-Z0-9-.]{2,256}\.[a-z]{2,20}(\:[0-9]{2,4})?\/[a-zA-Z0-9@:%_\+.~#?&\/\/=\-]*/i,
-		// Returns the file path from a URL
-		path: /^https?:\/\/[a-zA-Z0-9-.]{2,256}\.[a-z]{2,20}(\/[a-zA-Z0-9@:%_\+.~#?!&\/\/=\-]*)/i,
 		// IPv4 validation
 		ipv4: /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/g,
+		
+		// URL and IPv4 validation with hostname and path capture
+		urlAndIpv4: /^https?:\/\/([a-zA-Z0-9-.]{2,256}\.[a-z]{2,20}(?:\:[0-9]{2,4})?|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\:[0-9]{2,4})?)\/([a-zA-Z0-9@:%_\+.~#?&\/\/=\-]+)?/,
+		
 		// HTTP status codes
 		httpCodes: /^(200|201|202|203|301|302|303|307|308|400|401|403|404|405|406|408|409|411|412|413|414|415|418|429|431|451)$/
 	},
@@ -30,8 +33,10 @@ var global = {
 		'ws:'
 	],
 	hostNameBlackList: [
+		'archive.org',
 		'web.archive.org',
-		'127.0.0.1'
+		'127.0.0.1',
+		'localhost'
 	],
 	alertSounds: [
 		'sound-1.mp3',
