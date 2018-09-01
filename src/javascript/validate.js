ab5b0012180e728dd5e99acafa2b15be495c5dc2
Joseph <joseph@verifiedjoseph.com>
2018-09-01 18:22:57 +0100

    Replace nested if statements with try...catch
    Replace nested if statements with try..catch
    Rename variable status to valid

--- src/javascript/validate.js
+++ src/javascript/validate.js
@@ -10,47 +10,44 @@
  */
 function validate(url, callback) {
 
-	var status = true,
+	var valid = true,
 		hostname,
 		urlRegex = new RegExp(global.regex.urlAndIpv4);
-	
+
 	debug.log('Vaildating URL: ' + url);
-	
-	// Vaildate URL or IPv4 address using regular expression
-	if (urlRegex.test(url)) {
 
-		// Get hostname from URL with regex 
-		hostname = url.match(urlRegex);
-		
-		// Check for hostname in hostname black list
-		global.hostNameBlackList.find(function (host) {
-			
-			if (hostname[1] === host) {
-				
-				status = false;
-				debug.log('URL has a black listed host name: ' + host);
-				
-			}
+	try {
 
-		});
+		// Validate URL format
+		if (urlRegex.test(url) === false) {
 
-		if (status === true) {
-
-			debug.log('URL is valid: ' + url);
+			throw new Error('URL format is not valid: ' + url);
 
 		}
 
-	} else {
+		// Get hostname with regex
+		hostname = url.match(urlRegex);
 
-		status = false;
-		debug.log('URL format is not valid: ' + url);
+		// Check blacklist for hostname 
+		if (global.hostNameBlacklist.includes(hostname[1])) {
+
+			throw new Error('URL hostname blacklisted: ' + hostname[1]);
+
+		}
+
+		debug.log('URL is valid: ' + url);
+
+	} catch (exception) {
+
+		valid = false;
+		debug.log(exception.message);
 
 	}
 
 	/**
 	 * @callback validate~callback
-	 * @param {boolean} status.
+	 * @param {boolean} valid.
 	 */
-	callback(status);
+	callback(valid);
 
 }
