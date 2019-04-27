e9e5e70bcd5522b4c6f374afcb363620a301eccf
Joseph <joseph@verifiedjoseph.com>
2019-04-27 16:05:44 +0100

    Add Notify.note()

--- src/javascript/notify.js
+++ src/javascript/notify.js
@@ -7,7 +7,27 @@
 	Notify User
 */
 function Notify() {
-
 	
+	/**
+	 * Display a notification
+	 * @param {string} url
+	 * @param {string} title
+	 * @param {string} message
+	 */
+	this.note = function note(title, message) {
+		
+		if (settings.get('contextMenuNote') === true) {
+			
+			browser.notifications.create('', {
+				title: title,
+				iconUrl: '/images/icons/96.png',
+				type: 'basic',
+				message: message
+			});
+			
+			debug.log('Notification Displayed \n Title: ' + title + '\n Message: ' + message);
+		}
+		
+	};
 	
 }
