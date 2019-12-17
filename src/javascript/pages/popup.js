63fc5284c7990eddac1d57148d1b4eb8c950c880
Joseph <joseph@verifiedjoseph.com>
2019-12-17 22:52:09 +0000

    [popup.js] Use 'pendingUrl' tabs.query API property
    Since chrome 79 (Dec 2019), the property 'pendingUrl' is returned by the tabs.query API when a tab is loading. Firefox (71) does not currently support is property.

--- src/javascript/pages/popup.js
+++ src/javascript/pages/popup.js
@@ -266,9 +266,19 @@ browser.tabs.query({
 	currentWindow: true,
 	active: true
 }, function (tabs) {
+	var tab = tabs[0];
 
-	// Set URL
-	url = tabs[0].url;
+	console.log(tabs[0]);
+
+	/*
+		Since chrome 79 (Dec 2019), the property 'pendingUrl' is returned by the tabs.query API when a tab is loading.
+		Firefox (71) does not currently support is property.
+	*/
+	if (tab.status === 'loading' && tab.hasOwnProperty('pendingUrl')) {
+		url = tabs[0].pendingUrl;
+	} else {
+		url = tabs[0].url;
+	}
 
 	// Load setting and run the start function
 	settings.load(start);
